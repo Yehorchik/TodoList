32a8785098d9c138d63ac23d55fb47a23dcc991a
Yehor Melnyk <egorchikmelnyk@gmail.com>
2019-07-25 17:28:36 -0700

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Yehor Melnyk on 7/25/19.
+//  Copyright © 2019 Yehor Melnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
